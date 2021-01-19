59f84338ec93c8f0a26170971ff47926445bbca5
tecchy1997 <tetsu1031kato@gmail.com>
2021-01-19 14:24:18 +0000

    Initialize repository

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,42 @@
+class TodolistsController < ApplicationController
+  def new
+    @list = List.new
+  end
+  def create
+    # １. データを新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # ３. トップ画面へリダイレクト
+    redirect_to todolist_path(list.id)
+  end
+  def index
+    @lists = List.all
+  end
+  def show
+    @list = List.find(params[:id])
+  end
+  def edit
+    @list = List.find(params[:id])
+  end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
+  end
+  def back
+    @lists = List.all
+  end
+
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+end
